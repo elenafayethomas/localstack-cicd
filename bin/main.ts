e5e9e3e2af98a9c58f178f4d6cd0db0930c3e61b
Elena Thomas <elenafayethomas@eiu.com>
2024-01-19 14:35:16 +0000

    fix(cdk): change to using profile for CDK

--- bin/main.ts
+++ bin/main.ts
@@ -7,7 +7,7 @@ const app = new cdk.App();
 
 new LocalstackDemoStack(app, 'LocalstackCicdStack', {
     env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
+        account: cdk.Aws.ACCOUNT_ID,
+        region: cdk.Aws.REGION,
     }
 });
