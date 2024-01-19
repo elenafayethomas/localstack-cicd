31f3c503049e0e72a503b1d87fdaea2cf7ce3482
Elena Thomas <elenafayethomas@eiu.com>
2024-01-19 13:18:58 +0000

    fix(circleci): make deploy depend on install, remove private packages

--- bin/main.ts
+++ bin/main.ts
@@ -2,15 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { LocalstackDemoStack } from '../lib/localstack-demo-stack';
-import { BusinessUnitType, Enablement, EnvironmentType } from '@economist/enablement-constructs';
 
 const app = new cdk.App();
-
-new Enablement(app, {
-  serviceName: 'some-service',
-  environment: EnvironmentType.DEVELOPMENT,
-  businessUnit: BusinessUnitType.THE_ECONOMIST_GROUP,
-  squad: 'enablement',
-});
 
 new LocalstackDemoStack(app, 'LocalstackCicdStack');
